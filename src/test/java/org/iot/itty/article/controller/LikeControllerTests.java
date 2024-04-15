8cd5da7300dbba04895ddaa88f9c302c5e13a4d0
shindaeyeong <eodud3196@gmail.com>
2024-04-15 11:57:26 +0900

    fix: "branch change"
    jenkins실행 후

---
+++ src/test/java/org/iot/itty/article/controller/LikeControllerTests.java
@@ -0,0 +1,34 @@
+package org.iot.itty.article.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeControllerTests {
+
+	@Autowired
+	private LikeController likeController;
+
+	@Test
+	void selectAllLikeByUserCodeFk() {
+	}
+
+	@Test
+	void registArticleLike() {
+	}
+
+	@Test
+	void deleteArticleLike() {
+	}
+
+	@Test
+	void registReplyLike() {
+	}
+
+	@Test
+	void deleteReplyLike() {
+	}
+}
