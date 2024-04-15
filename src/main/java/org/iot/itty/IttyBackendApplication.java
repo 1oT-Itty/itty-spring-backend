7c159bb35ba23ed1c34bba8d8efe948339f235c7
bucky1005 <null00007@naver.com>
2024-04-15 15:30:41 +0900

    test: "jenkins test"
    jenkins test

--- src/main/java/org/iot/itty/IttyBackendApplication.java
+++ src/main/java/org/iot/itty/IttyBackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class IttyBackendApplication {
 
+	// main push test
 	public static void main(String[] args) {
 		SpringApplication.run(IttyBackendApplication.class, args);
 	}
